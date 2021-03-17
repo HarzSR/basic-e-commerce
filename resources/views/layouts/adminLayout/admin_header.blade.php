b703620574586547afe8ddcadb7b2e87a2eaf46b
Hariharan Thiagarajan <hariharansmm@gmail.com>
2021-03-18 12:31:14 +1300

    Twenty Sixth Commit
    Downloaded Free E Commerce Site
    Placed CSS into frontend_css
    Placed JS into frontend_js
    Places Images into frontend_images
    Placed Fonts into frontend_fonts
    
    Created Frontend Layout
    front_header.blade
    front_design.blade
    front_footer.blade
    
    Copied HTML from downloaded E Commerce
    Fixed Link to CSS, JS, Images, Fonts

--- resources/views/layouts/adminLayout/admin_header.blade.php
+++ resources/views/layouts/adminLayout/admin_header.blade.php
@@ -4,7 +4,6 @@
 </div>
 <!--close-Header-part-->
 
-
 <!--top-Header-menu-->
 <div id="user-nav" class="navbar navbar-inverse">
     <ul class="nav">
@@ -12,20 +11,7 @@
             <ul class="dropdown-menu">
                 <li><a href="{{ url('/admin/settings') }}"><i class="icon-user"></i> My Profile</a></li>
                 <li class="divider"></li>
-                <li><a href="#"><i class="icon-check"></i> My Tasks</a></li>
-                <li class="divider"></li>
                 <li><a href="{{ url('/logout') }}"><i class="icon-key"></i> Log Out</a></li>
-            </ul>
-        </li>
-        <li class="dropdown" id="menu-messages"><a href="#" data-toggle="dropdown" data-target="#menu-messages" class="dropdown-toggle"><i class="icon icon-envelope"></i> <span class="text">Messages</span> <span class="label label-important">5</span> <b class="caret"></b></a>
-            <ul class="dropdown-menu">
-                <li><a class="sAdd" title="" href="#"><i class="icon-plus"></i> new message</a></li>
-                <li class="divider"></li>
-                <li><a class="sInbox" title="" href="#"><i class="icon-envelope"></i> inbox</a></li>
-                <li class="divider"></li>
-                <li><a class="sOutbox" title="" href="#"><i class="icon-arrow-up"></i> outbox</a></li>
-                <li class="divider"></li>
-                <li><a class="sTrash" title="" href="#"><i class="icon-trash"></i> trash</a></li>
             </ul>
         </li>
         <li class=""><a title="" href="{{ url('/admin/settings') }}"><i class="icon icon-cog"></i> <span class="text">Settings</span></a></li>
--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('/', 'IndexController@index');
 
 // Route::get('/admin','AdminController@login');
 Route::match(['get', 'post'], '/admin', 'AdminController@login');
